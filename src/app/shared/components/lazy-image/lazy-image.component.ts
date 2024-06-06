7a10861c846035db83842a49904d83f01bb37258
MarceloYevenes <marcelo.yevenes.moreno@gmail.com>
2024-06-06 12:57:27 -0400

    Agregada las animaciones

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, input } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('Url property is required');
+  }
+
+  onLoad() {
+    setTimeout(() => {
+      this.hasLoaded = true;
+    }, 1000);
+
+  }
 
 }
